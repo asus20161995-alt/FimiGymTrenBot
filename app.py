3bd977e9ea2f1db347f88770eebb2629b818d23a
asus20161995-alt <asus20161995@gmail.com>
2025-08-08 07:27:35 +1100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+
+from flask import Flask, request
+import telegram
+import os
+
+app = Flask(__name__)
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telegram.Bot(token=TOKEN)
+
+@app.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    data = request.get_json()
+    if "message" in data:
+        chat_id = data["message"]["chat"]["id"]
+        text = data["message"].get("text", "")
+        bot.sendMessage(chat_id=chat_id, text="Привет! Это бот FimiGymTrenBot. Скоро начнем тренировку!")
+    return "ok"
+
+@app.route('/')
+def index():
+    return "Бот работает!"
+
+if __name__ == '__main__':
+    app.run()
